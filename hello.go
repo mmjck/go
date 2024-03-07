a1a6517c953b14013ad7a9c0bcd75460ac580e65
mmjck <msantosjck@gmail.com>
2024-03-07 09:52:52 -0400

    more examples

--- hello.go
+++ hello.go
@@ -6,4 +6,31 @@ func main() {
 	fmt.Println("Hello, World!")
 
 	beyondHello()
+	learnTypes()
+
+	p, q := learnMemory()
+
+	fmt.Println(*p, *q)
+
+	m := map[string]int{"três": 3, "quatro": 4}
+	m2 := map[string]bool{"f": false, "t": true}
+
+	fmt.Println(m2)
+	fmt.Println(m)
+
+	m["um"] = 1
+
+	fmt.Println(m)
+
+	_, _ = learnMemory()
+
+	learnFlowControl()
+
+	rectangle := Rectangle{width: 5, height: 3}
+	circle := Circle{radius: 4}
+
+	// Calculate and print areas
+	fmt.Println("Area of rectangle:", getArea(rectangle))
+	fmt.Println("Area of circle:", getArea(circle))
+
 }
