1fa8b8f06d90ef32de72220f6a2ac2e1102c9e39
mmjck <msantosjck@gmail.com>
2024-06-07 16:52:44 -0400

    generic models

---
+++ authentication/models/genericModels.go
@@ -0,0 +1,15 @@
+package models
+
+const (
+	ErrorTypeFatal        = "Fatal"
+	ErrorTypeError        = "Error"
+	ErrorTypeValidation   = "Validation Error"
+	ErrorTypeInfo         = "Info"
+	ErrorTypeDebug        = "Debug"
+	ErrorTypeUnauthorized = "Unauthorized"
+)
+
+type ErrorDetail struct {
+	ErrorType    string
+	ErrorMessage string
+}
