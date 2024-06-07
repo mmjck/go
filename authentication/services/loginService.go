114b30207d3ef602bc2b3fa42a7b7adb3dd9aeac
mmjck <msantosjck@gmail.com>
2024-06-07 18:00:03 -0400

    added getToken function

--- authentication/services/loginService.go
+++ authentication/services/loginService.go
@@ -3,7 +3,12 @@ package services
 import (
 	"authentication/models"
 	"authentication/repository"
+	"authentication/token"
 	"log"
+	"strconv"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 type Login struct {
@@ -18,4 +23,32 @@ func NewLogin(l *log.Logger, f *models.Flags) *Login {
 		flags:           f,
 		loginRepository: repository.Init(),
 	}
+}
+
+func (l *Login) GetToken(model models.LoginRequest, origin string) (string, *models.ErrorDetail) {
+	user, err := l.loginRepository.GetUserByName(model.UserName, model.Password)
+
+	if err != nil {
+		return "", err
+	}
+
+	var claims = &models.JwtClaims{
+		ComapnyId: strconv.Itoa(user.Id),
+		Username:  user.Name,
+		Roles:     user.Roles,
+		StandardClaims: jwt.StandardClaims{
+			Audience: origin,
+		},
+	}
+
+	var tokenCreationTime = time.Now().UTC()
+
+	var expirationTime = tokenCreationTime.Add(time.Duration(2) * time.Hour)
+
+	return token.GenerateToken(claims, expirationTime)
+
+}
+
+func (*Login) VerifyToken(tokenString, referer string) (bool, *models.JwtClaims) {
+	return token.VerifyToken(tokenString, referer)
 }
