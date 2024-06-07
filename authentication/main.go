c2e63e89d304a9edac15d57de7b1d22a4dd2804a
mmjck <msantosjck@gmail.com>
2024-06-07 16:51:46 -0400

    main

--- authentication/main.go
+++ authentication/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"authentication/models"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+var (
+	port = flag.String("port", "8080", "port to be used")
+	ip   = flag.String("ip", "localhost", "ip to be used")
+)
 
 func main() {
+	flag.Parse()
+	flags := models.NewFlags(*ip, *port)
 	fmt.Println("Hello, world.")
+
+	logger := log.New(os.Stdout, "auth", 1)
+	route := route.Regi
 }
