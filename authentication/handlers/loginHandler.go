e23fa0c57717da5bf0de924730226fd7a64fb574
mmjck <msantosjck@gmail.com>
2024-06-07 16:52:14 -0400

    login handler

---
+++ authentication/handlers/loginHandler.go
@@ -0,0 +1,65 @@
+package handlers
+
+import (
+	"authentication/models"
+	"authentication/services"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Login struct {
+	logger       *log.Logger
+	flags        *models.Flags
+	loginService *services.Login
+}
+
+func NewLogin(l *log.Logger, f *models.Flags) *Login {
+	loginService := services.Login(l, f)
+
+	return &Login{
+		logger:       l,
+		flags:        f,
+		loginService: loginService,
+	}
+}
+
+func (l *Login) Login(context *gin.Context) {
+	var loginObj models.LoginRequest
+
+	if err := context.ShouldBindJSON(&loginObj); err != nil {
+		var errors []models.ErrorDetail = make([]models.ErrorDetail, 0, 1)
+
+		errors = append(errors, models.ErrorDetail{
+			ErrorType:    models.ErrorTypeValidation,
+			ErrorMessage: fmt.Sprintf("%v", err),
+		})
+
+		badRequest(context, http.StatusBadRequest, "invalid request", errors)
+		return
+	}
+	tokeString, err := l.loginService.GetToken(loginObj, context.Request.Header.Get("Referer"))
+
+	if err != nil {
+		badRequest(context, http.StatusBadRequest, "error in gerating tolen", []models.ErrorDetail{
+			*err,
+		})
+		return
+	}
+
+	ok(context, http.StatusOK, "token created", tokeString)
+}
+
+func (l *Login) VerifyToken(conext *gin.Context) {
+	tokenString := conext.Request.Header.Get("apiKey")
+	referer := conext.Request.Header.Get("Referer")
+
+	valid, claims := l.loginService.VerifyToken(tokenString, referer)
+	if !valid {
+		returnUnauthorized(context)
+		return
+	}
+	ok(context, http.StatusOK, "token is valid", claims)
+}
