d1af43d51f7505699fc3c793a9c66cfb0ad41c0d
mmjck <msantosjck@gmail.com>
2024-06-09 10:31:41 -0400

    new route

--- authentication/routers/routers.go
+++ authentication/routers/routers.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"authentication/handlers"
 	"authentication/models"
+	"authentication/token"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -17,5 +18,18 @@ type Login struct {
 func (r *Login) RegisterRoutes() *gin.Engine {
 	ginEngine := gin.Default()
 	ginEngine.POST("/login", r.loginHandler.Login)
+	ginEngine.POST("/validate-token", r.loginHandler.VerifyToken)
 
+	return ginEngine
+}
+
+func NewRoute(l *log.Logger, f *models.Flags) *Login {
+	loginHandler := handlers.NewLogin(l, f)
+
+	token.Init()
+	return &Login{
+		logger:       l,
+		loginHandler: loginHandler,
+		flags:        f,
+	}
 }
