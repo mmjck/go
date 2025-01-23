642a2cabff92460c3b2ee7700a87c968ba372bf1
Jackson <jackson.santos@conecthus.org.br>
2025-01-23 16:05:59 -0400

    add separate files for route handlers

---
+++ handler/handler.go
@@ -0,0 +1,12 @@
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+
+
+
+
+
--- router/routes.go
+++ router/routes.go
@@ -1,7 +1,7 @@
 package router
 
 import (
-	"net/http"
+	"gopportunities/handler"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,34 +10,14 @@ func initializeRoutes(r *gin.Engine) {
 	v1 := r.Group("/api/v1")
 
 	{
-		v1.GET("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "GET opening",
-			})
-		})
+		v1.GET("/opening", handler.ShowOpeningHandler)
 
-		v1.POST("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "POST opening",
-			})
-		})
+		v1.POST("/opening", handler.CreateOpeningHandler)
 
-		v1.PUT("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "PUT opening",
-			})
-		})
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
 
-		v1.DELETE("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "DELETE opening",
-			})
-		})
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
 
-		v1.GET("/openings", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "GET opening",
-			})
-		})
+		v1.GET("/openings", handler.ListOpeningHandler)
 	}
 }
