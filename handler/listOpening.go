fab65173b5341096a0f2a6bf9582bc110735a91e
Jackson <jackson.santos@conecthus.org.br>
2025-02-01 18:24:41 -0400

    list opening

--- handler/listOpening.go
+++ handler/listOpening.go
@@ -1,13 +1,18 @@
 package handler
 
 import (
+	"gopportunities/schemas"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET opening",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+	sendSuccess(ctx, "list-openings", openings)
 }
