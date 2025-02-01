bb1130cca25d7f260d420149284975358a4dacd5
Jackson <jackson.santos@conecthus.org.br>
2025-02-01 18:24:58 -0400

    show opening by id

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -1,13 +1,27 @@
 package handler
 
 import (
+	"fmt"
+	"gopportunities/schemas"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET opening",
-	})
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found", id))
+		return
+	}
+
+	sendSuccess(ctx, "show-opening", opening)
+
 }
